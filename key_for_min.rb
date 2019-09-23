618d6b1bb4011ec6f58daa86d5e605fe56824560
James Benjamin <james.abenj@gmail.com>
2019-09-23 16:34:26 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,4 +3,15 @@
 
 def key_for_min_value(name_hash)
 
+new_array = name_hash.collect do |key, value|
+  value
+end
+new_array.sort 
+name_hash.each do |key, value|
+    if value == new_array[0]
+       return key  
+    end
+  end 
+  name_hash.empty?
+  name_hash = nil
 end
